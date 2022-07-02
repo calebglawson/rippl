3d4aab702ac4dde4a14f87a47820853747531a74
Caleb Lawson <caleb.g.lawson@gmail.com>
2022-07-02 16:01:57 -0400

    syntax

--- main.go
+++ main.go
@@ -22,18 +22,17 @@ type Downloader struct {
 }
 
 func (d *Downloader) run(ctx context.Context) {
-		if len(d.queue) > 0 {
-			d.flush()
-		}
-
-		select {
-		case <-ctx.Done():
-
-			return
-		}
-
-		time.Sleep(d.interval)
+	if len(d.queue) > 0 {
+		d.flush()
 	}
+
+	select {
+	case <-ctx.Done():
+
+		return
+	}
+
+	time.Sleep(d.interval)
 }
 
 func (d *Downloader) flush() {
